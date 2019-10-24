298ce9b16bf82833cd07c8b415d1da42eb506ab8
Gbenga Oshinaga <goshinaga@yahoo.com>
2019-10-24 18:00:09 +0000

    add files to gitignore

--- src/main/java/constants/Speech.java
+++ src/main/java/constants/Speech.java
@@ -4,8 +4,8 @@ public final class Speech {
 
   private Speech() {
   }
-  
-  
+
+
   /*=============================================================================
    * Common Statements
    ============================================================================*/
@@ -15,21 +15,19 @@ public final class Speech {
   public static final String CREATOR = "I was created by a dude called Gbenga Oshinaga";
   public static final String GENDER = "I have none, I'm a computer program";
   public static final String CAN_DO = "Well, I can:\n"
-      + "Perform searches for you\n"
-      + "View and create events in your google calendar\n"
       + "Define words\n"
       + "Get wikipedia introduction text on a title(P.S. You have to add the word 'wiki' to the start or end of your statement)\n"
       + "That's basically it for now...";
   public static final String YOU_WELCOME = "you're welcome";
   public static final String[] OK = {"okay","cool","yea","ok"};
-  
-  
+
+
   /*=============================================================================
    * Common Greetings And Response to Greetings
    ============================================================================*/
-  public static final String[] GREETINGS = {"whats up", "hello", "hey", 
+  public static final String[] GREETINGS = {"whats up", "hello", "hey",
       "how far", "how are you", "how are you?",
       "hi", "how far?", "what's up", "what's up?", "whats up?", "wats up"};
-  public static final String[] RESPONSE_TO_GREETINGS = {"I dey", "I'm fine", "I'm cool", 
+  public static final String[] RESPONSE_TO_GREETINGS = {"I dey", "I'm fine", "I'm cool",
       "I'm okay", "I'm awesome"};
 }
